11c3e8b2c0ed4434d5ea723f97423238760205bc
Norman Cooke <Norman.Cooke@Honeywell.com>
2020-08-20 13:07:05 -0500

    begin minor cleanup post isc490
    added pipenv, pycharm com.ed. settings, and settings.json to add/store settings and whatnot.

--- sn-dwnldr.py
+++ sn-dwnldr.py
@@ -1,53 +1,58 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-#
-#------------------SETUP_&_INITIAL_VARIABLES------------------------------------
+# sn-dwnldr.py
+# https://github.com/ndcooke/sn-dwnldr
+# forked from https://github.com/thepunkgeek/sn-dwnldr a while back
+# A downloader for security now podcast. could probably do others.
 
-import urllib.request, urllib.parse, urllib.error
+
 import http.client
-import time 
 import os.path
-import xml.etree.ElementTree as ET
+import time
+import urllib.error
+import urllib.parse
+import urllib.request
+import xml.etree.ElementTree as ElTree
 
-save_directory="D:\\SecurityNow\\"
-last_episode=751
+save_directory = "D:\\SecurityNow\\"
+last_episode = 751
 
-base_url='http://twit.cachefly.net/audio/sn/'
+base_url = 'http://twit.cachefly.net/audio/sn/'
 
 # now we can get the latest episode straight from the rss feed
-xml_url='http://www.leoville.tv/podcasts/sn.xml'
-xmltree = ET.parse(urllib.request.urlopen(xml_url))
-xmldoc = xmltree.getroot()
+url = 'http://www.leoville.tv/podcasts/sn.xml'
+tree = ElTree.parse(urllib.request.urlopen(url))
+doc = tree.getroot()
+
 # declare namespaces that exist in the feed (as of 2019-06-03)
-namespaces = {'atom':'http://www.w3.org/2005/Atom'
-              ,'media':'http://search.yahoo.com/mrss/'
-              ,'itunes':'http://www.itunes.com/dtds/podcast-1.0.dtd'
-              ,'creativeCommons':'http://backend.userland.com/creativeCommonsRssModule'
-              ,'content':'http://purl.org/rss/1.0/modules/content/'
-              ,'sy':'http://purl.org/rss/1.0/modules/syndication/'}
+namespaces = dict(atom='http://www.w3.org/2005/Atom', media='http://search.yahoo.com/mrss/',
+                  itunes='http://www.itunes.com/dtds/podcast-1.0.dtd',
+                  creativeCommons='http://backend.userland.com/creativeCommonsRssModule',
+                  content='http://purl.org/rss/1.0/modules/content/',
+                  sy='http://purl.org/rss/1.0/modules/syndication/')
 
 # get the largest episode number
-results = xmltree.findall('.//itunes:episode', namespaces)
+results = tree.findall('.//itunes:episode', namespaces)
 for result in results:
     if int(result.text) > last_episode:
-        last_episode=int(result.text)
+        last_episode = int(result.text)
+
 
 def ensure_dir(f):
     d = os.path.dirname(f)
     if not os.path.exists(d):
         os.makedirs(d)
+
+
 ensure_dir(save_directory)
 
 while os.path.isfile('sn_dwnldr.log') == False:
-	print()
-	print('Creating Log')
-	f = open("sn_dwnldr.log", 'w') 
-	f.write('1')
-	f = open("sn_dwnldr.log", 'r')
-	log = f.read()
-	f.close()
-	print() #'--------------------------------------------------------------------------------'
-	
+    print()
+    print('Creating Log')
+    f = open("sn_dwnldr.log", 'w')
+    f.write('1')
+    f = open("sn_dwnldr.log", 'r')
+    log = f.read()
+    f.close()
+    print()  # '--------------------------------------------------------------------------------'
 
 f = open("sn_dwnldr.log", 'r+')
 log = f.read()
@@ -68,53 +73,53 @@ print('The episodes will be downloaded to:' + save_directory)
 print()
 print('-------------------BEGIN-DOWNLOAD-----------------------------------------------')
 print()
-	
+
 print('Will start downloading at: Episode ' + number)
 print('Will finish downloading at: Episode ' + str(last_episode))
 
-while 10>i:
-	number = str(i)
-	episode_number = '000' + number
-	name = 'sn' + episode_number 
-	file_name = name + '.mp3'
-	full_url = base_url + name + '/' + file_name
-	urllib.request.urlretrieve(full_url, save_directory + file_name)
-	i = i + 1
-	f = open('sn_dwnldr.log', 'w') #creates or overwrites log file
-	f.write(episode_number) #logs the next episode that needs to be downloaded
-	f.close()
-	print(file_name + ' was downloaded')
-	time.sleep(15) #slows it down so that CacheFly doesn't disconnect you, you can try less time 
-	print('--------------------------------------------------------------------------------')
+while 10 > i:
+    number = str(i)
+    episode_number = '000' + number
+    name = 'sn' + episode_number
+    file_name = name + '.mp3'
+    full_url = base_url + name + '/' + file_name
+    urllib.request.urlretrieve(full_url, save_directory + file_name)
+    i = i + 1
+    f = open('sn_dwnldr.log', 'w')  # creates or overwrites log file
+    f.write(episode_number)  # logs the next episode that needs to be downloaded
+    f.close()
+    print(file_name + ' was downloaded')
+    time.sleep(15)  # slows it down so that CacheFly doesn't disconnect you, you can try less time
+    print('--------------------------------------------------------------------------------')
 
-while i<100:
-	number = str(i)
-	episode_number = '00' + number
-	name = 'sn' + episode_number 
-	file_name= name + '.mp3'
-	full_url= base_url + name + '/' + file_name
-	urllib.request.urlretrieve(full_url, save_directory + file_name )
-	i = i + 1
-	f = open('sn_dwnldr.log', 'w') #creates or overwrites log file
-	f.write(episode_number) #logs the next episode that needs to be downloaded
-	f.close()
-	print(file_name + ' was downloaded')
-	time.sleep(15)
-	print('--------------------------------------------------------------------------------')
+while i < 100:
+    number = str(i)
+    episode_number = '00' + number
+    name = 'sn' + episode_number
+    file_name = name + '.mp3'
+    full_url = base_url + name + '/' + file_name
+    urllib.request.urlretrieve(full_url, save_directory + file_name)
+    i = i + 1
+    f = open('sn_dwnldr.log', 'w')  # creates or overwrites log file
+    f.write(episode_number)  # logs the next episode that needs to be downloaded
+    f.close()
+    print(file_name + ' was downloaded')
+    time.sleep(15)
+    print('--------------------------------------------------------------------------------')
 
-while i <= int(last_episode): #this is the most current episode number, where it stops
-	number = str(i)
-	episode_number = '0' + number 
-	name = 'sn' + episode_number 
-	file_name = name + '.mp3'
-	full_url = base_url + name + '/' + file_name
-	urllib.request.urlretrieve(full_url, save_directory + file_name)
-	i = i + 1
-	f = open('sn_dwnldr.log', 'w') #creates or overwrites log file
-	f.write(episode_number) #logs the next episode that needs to be downloaded
-	f.close()
-	print(file_name + ' was downloaded')
-	time.sleep(15)
-	print('--------------------------------------------------------------------------------')
+while i <= int(last_episode):  # this is the most current episode number, where it stops
+    number = str(i)
+    episode_number = '0' + number
+    name = 'sn' + episode_number
+    file_name = name + '.mp3'
+    full_url = base_url + name + '/' + file_name
+    urllib.request.urlretrieve(full_url, save_directory + file_name)
+    i = i + 1
+    f = open('sn_dwnldr.log', 'w')  # creates or overwrites log file
+    f.write(episode_number)  # logs the next episode that needs to be downloaded
+    f.close()
+    print(file_name + ' was downloaded')
+    time.sleep(15)
+    print('--------------------------------------------------------------------------------')
 
 print('Finished downloading all Security Now episodes')
